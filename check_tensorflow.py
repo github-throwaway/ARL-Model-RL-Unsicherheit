8f8b6d766534fb96a0204ecb73a3d18c6303ac7c
gstaber <georg.staber@ead-systeme.de>
2021-06-23 11:47:59 +0200

    Added tf check

---
+++ check_tensorflow.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import os, sys
+
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+config = tf.compat.v1.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.95
+#config.gpu_options.visible_device_list = "1"
+tf.compat.v1.Session(config=config)
+
+
+print("python {}".format(sys.version))
+print("tensorflow version {}".format(tf.__version__))
