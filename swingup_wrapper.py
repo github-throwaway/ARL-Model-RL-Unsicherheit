9e500f895152694b505535dcb2a73f678fa10636
gstaber <georg.staber@ead-systeme.de>
2021-07-27 16:46:09 +0200

    - Updated a few todos

--- swingup_wrapper.py
+++ swingup_wrapper.py
@@ -5,10 +5,11 @@ from random import uniform
 
 import gym
 import numpy as np
-from gym_cartpole_swingup.envs.cartpole_swingup import CartPoleSwingUpEnv
+from gym_cartpole_swingup.envs.cartpole_swingup import CartPoleSwingUpEnv, CartPoleSwingUpV1, CartPoleSwingUpV0
 
 
 class SwingUpWrapper(gym.Env):
+
     # Could be one of:
     # CartPoleSwingUp-v0, CartPoleSwingUp-v1
     # If you have PyTorch installed:
@@ -17,43 +18,50 @@ class SwingUpWrapper(gym.Env):
         self.min_range = min_range
         self.max_range = max_range
         self.offset = offset
-        self.org_env = CartPoleSwingUpEnv()
-        #todo: action_space und observatino_space definieren
+        self.org_env = CartPoleSwingUpV0()
+        self.action_space = gym.Env.action_space
+        self.observation_space = gym.Env.observation_space
 
     def reset(self):
         state = self.org_env.reset()
         State = namedtuple("State", "x_pos x_dot theta theta_dot")
-        random_theta = np.random.uniform(0.0,2.0*np.pi)
-        self.org_env.state = State(state.x_pos,state.x_dot,random_theta,state.theta_dot)
+        random_theta = np.random.uniform(0.0, 2.0 * np.pi)
+        self.org_env.state = State(state[0], state[1], random_theta, state[4])
         return self.org_env.state
     # todo track position auch zufällig?
 
-    def step(self,action):
+    def step(self, action):
         # observation = [x_pos, x_dot, np.cos(theta),np.sin(theta),theta_dot]
         observation, reward, done, info = self.org_env.step(action)
-        pole_angle = math.atan(observation[3]/observation[2])
+        pole_angle = math.atan(observation[3] / observation[2])
 
         my_obs = list(observation)
         if self.min_range < pole_angle < self.max_range:
-            fake_observation = pole_angle + uniform(-self.offset,self.offset)
+            fake_observation = pole_angle + uniform(-self.offset, self.offset)
             # todo: verrauschten winkel & Winkelgeschwindigkeit ersetzen
             # differenz zwischen winkel von winkelgeschwindigkeit abziehen
             # nur zurück liefern, nicht im original env verändern!
+            fake_angular_velocity = observation[4]-(pole_angle - fake_observation)
             my_obs.append(fake_observation)
+
+            reward = 1 - abs(observation[0]) + np.cos(fake_observation)
             info["uncertain"] = True
         else:
             my_obs.append(pole_angle)
             info["uncertain"] = False
 
             # todo: Reward = cos(theta_noise, x_pos)
+            # Reward == Hypotenuse
+            reward = 1 - abs(observation[0]) + np.cos(pole_angle)
             # siehe python notebook aus übung
 
         return my_obs, reward, done, info
 
+
 if __name__ == "__main__":
     env = SwingUpWrapper()
     done = False
-    env.reset()
+    state = env.reset()
 
     while not done:
         action = env.org_env.action_space.sample()
