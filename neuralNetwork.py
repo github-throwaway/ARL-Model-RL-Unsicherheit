7184af5ac4d4f598afa6273bb69c11e3d633568f
sophiasigethy <sophia@sigethy.de>
2021-08-02 14:02:50 +0200

    neural network expanded

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -8,15 +8,23 @@ import numpy as np
 
 
 import tensorflow as tf
-#import tensorflow.keras.backend as K
+#todo for expanded
+#import tensorflow.compat.v2 as tf
+
 keras = tf.keras
 K = keras.backend
 
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras import optimizers
+
 KL = keras.optimizers
+KD = keras.Sequential
 import tensorflow_probability as tfp
 tfd = tfp.distributions
+
+#todo for expanded
+#from tensorflow_probability.python.layers.dense_variational_v2 import _make_kl_divergence_penalty
+
 
 
 negloglik = lambda y, p_y: -p_y.log_prob(y)
@@ -71,7 +79,7 @@ def prior_trainable(kernel_size, bias_size=0, dtype=None):
 
 def neuralNetworkExpanded():
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(1),
+        tf.layers.DenseVariational(1, posterior_mean_field, prior_trainable, kl_weight=1 / data1.shape[0]),
         tfp.layers.DistributionLambda(lambda t: tfd.Normal(loc=t, scale=1)),
     ])
 
@@ -90,13 +98,95 @@ def neuralNetworkExpanded():
     # print(model.get_weights()[1])
     yhat = model(x_tst)
     print(K.eval(yhat.mean()))
+    print(K.eval(yhat.variance()))
     # print("------")
     # assert isinstance(yhat, tfd.Distribution)
     # test_mult=tf.math.multiply(tf.transpose(x_tst),model.weights[0])
     # print(K.eval(test_mult))
 
 
+class DenseVariational(tf.keras.layers.Layer):
+    def __init__(self,
+                 units,
+                 make_posterior_fn,
+                 make_prior_fn,
+                 kl_weight=None,
+                 kl_use_exact=False,
+                 activation=None,
+                 use_bias=True,
+                 activity_regularizer=None,
+                 **kwargs):
 
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        super(DenseVariational, self).__init__(
+            activity_regularizer=tf.keras.regularizers.get(activity_regularizer),
+            **kwargs)
+        self.units = int(units)
+
+        self._make_posterior_fn = make_posterior_fn
+        self._make_prior_fn = make_prior_fn
+        self._kl_divergence_fn = _make_kl_divergence_penalty(
+            kl_use_exact, weight=kl_weight)
+
+        self.activation = tf.keras.activations.get(activation)
+        self.use_bias = use_bias
+        self.supports_masking = False
+        self.input_spec = tf.keras.layers.InputSpec(min_ndim=2)
+
+    def build(self, input_shape):
+        dtype = tf.as_dtype(self.dtype or tf.keras.backend.floatx())
+        if not (dtype.is_floating or dtype.is_complex):
+            raise TypeError('Unable to build `Dense` layer with non-floating point '
+                            'dtype %s' % (dtype,))
+        input_shape = tf.TensorShape(input_shape)
+        last_dim = tf.compat.dimension_value(input_shape[-1])
+        if last_dim is None:
+            raise ValueError('The last dimension of the inputs to `DenseVariational` '
+                             'should be defined. Found `None`.')
+        self.input_spec = tf.keras.layers.InputSpec(
+            min_ndim=2, axes={-1: last_dim})
+
+        self._posterior = self._make_posterior_fn(
+            last_dim * self.units,
+            self.units if self.use_bias else 0,
+            dtype)
+        self._prior = self._make_prior_fn(
+            last_dim * self.units,
+            self.units if self.use_bias else 0,
+            dtype)
+
+        self.built = True
+
+    def call(self, inputs):
+        dtype = tf.as_dtype(self.dtype or tf.keras.backend.floatx())
+        inputs = tf.cast(inputs, dtype, name='inputs')
+
+        q = self._posterior(inputs)
+        r = self._prior(inputs)
+        self.add_loss(self._kl_divergence_fn(q, r))
+
+        w = tf.convert_to_tensor(value=q)
+        prev_units = self.input_spec.axes[-1]
+        if self.use_bias:
+            split_sizes = [prev_units * self.units, self.units]
+            kernel, bias = tf.split(w, split_sizes, axis=-1)
+        else:
+            kernel, bias = w, None
+
+        kernel = tf.reshape(kernel, shape=tf.concat([
+            tf.shape(kernel)[:-1],
+            [prev_units, self.units],
+        ], axis=0))
+        outputs = tf.matmul(inputs, kernel)
+
+        if self.use_bias:
+            outputs = tf.nn.bias_add(outputs, bias)
+
+        if self.activation is not None:
+            outputs = self.activation(outputs)  # pylint: disable=not-callable
+
+        return outputs
 
 
 if __name__ == "__main__":
@@ -112,6 +202,9 @@ if __name__ == "__main__":
     #print(data2)
 
     neuralNetworkSimple()
+
+    #denseVar= DenseVariational(1, posterior_mean_field, prior_trainable, kl_weight=1 / data1.shape[0]), tfp.layers.DistributionLambda(lambda t: tfd.Normal(loc=t, scale=1))
+    #neuralNetworkExpanded()
 
 
 
