91acebabf4e1015707b2c17bdffbd541c4056f7c
Simon Lund <simon.lund@campus.lmu.de>
2021-09-01 13:03:31 +0200

    add changes from branch /neuralNetwork

--- swingup_wrapper.py
+++ swingup_wrapper.py
@@ -2,8 +2,6 @@
 import math
 import pandas as pd
 from collections import namedtuple
-from random import uniform, choice
-from numpy import genfromtxt
 import gym
 import numpy as np
 
@@ -11,17 +9,10 @@ import numpy as np
 import tensorflow as tf
 keras = tf.keras
 K = keras.backend
-from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras import optimizers
+
 KL = keras.optimizers
 import tensorflow_probability as tfp
 tfd = tfp.distributions
-
-
-from gym_cartpole_swingup.envs.cartpole_swingup import CartPoleSwingUpEnv, CartPoleSwingUpV1, CartPoleSwingUpV0
-
-
-
 
 
 class SwingUpWrapper(gym.Env):
@@ -30,7 +21,8 @@ class SwingUpWrapper(gym.Env):
     # CartPoleSwingUp-v0, CartPoleSwingUp-v1
     # If you have PyTorch installed:
     # TorchCartPoleSwingUp-v0, TorchCartPoleSwingUp-v1
-    def __init__(self, offset=0.05, min_range=0.56, max_range=math.pi/2):
+    def __init__(self, offset=1, min_range=0.56 + math.pi, max_range=(math.pi/2)*3):
+    #def __init__(self, offset=1, min_range=0, max_range=2* math.pi):
         self.min_range = min_range
         self.max_range = max_range
         self.offset = offset
@@ -63,21 +55,24 @@ class SwingUpWrapper(gym.Env):
         # 270 grad entsprechen 0,5pi = ca 1,5
         # dh wenn er senkrecht leicht nach links bzw rechts pendelt, springt er zwischen 0 und ca 6 hin und her
         if observation[3]>0:
-            pole_angle = math.acos(observation[2])
+            pole_angle = (math.acos(observation[2]) + math.pi) % (math.pi*2)
         else:
-            pole_angle = math.acos(observation[2]*-1)+math.pi
+            pole_angle = (math.acos(observation[2]*-1)+math.pi * 2) % (math.pi*2)
 
         my_obs = list(observation)
         if self.min_range < pole_angle < self.max_range:
             #fake_observation = pole_angle + uniform(-self.offset, self.offset)
-            fake= np.random.normal(0, self.offset, 1)
+            fake = np.random.normal(0, self.offset, 1)
             fake_observation = pole_angle + fake[0]
             fake_observation= checkBoundaries(fake_observation)
             # todo: verrauschten winkel & Winkelgeschwindigkeit ersetzen
             # differenz zwischen winkel von winkelgeschwindigkeit abziehen
             # nur zurück liefern, nicht im original env verändern!
-            fake_angular_velocity = observation[4] - (pole_angle - fake_observation)
+            #fake_angular_velocity = observation[4] - (pole_angle - fake_observation)
+            fake_angular_velocity = observation[4] + fake[0]*100 - (pole_angle - fake_observation)
+            my_obs.append(fake_angular_velocity)
             my_obs.append(fake_observation)
+
 
 
             #reward = 1 - abs(observation[0]) + np.cos(fake_observation)
@@ -85,7 +80,9 @@ class SwingUpWrapper(gym.Env):
             #my_obs.append(reward)
             info["uncertain"] = True
         else:
+            my_obs.append(observation[4])
             my_obs.append(pole_angle)
+
             info["uncertain"] = False
 
             # todo: Reward = cos(theta_noise, x_pos)
@@ -114,7 +111,7 @@ if __name__ == "__main__":
     env = SwingUpWrapper()
 
     #100 durchläufe werden in csv gespeichert
-    for _ in range(15):
+    for _ in range(10):
 
         done = False
         state = env.reset()
@@ -136,7 +133,9 @@ if __name__ == "__main__":
             #observations.append(obs)
             observations.append(obs[0])
             observations.append(obs[1])
-            observations.append(obs[4])
+            #append fake winkelgeschwindigkeit!!!!!
+            #observations.append(obs[4])
+            observations.append(obs[5])
             # ONLY WHEN ACTION IS SAVED TOO
             observations.append(action[0])
 
@@ -151,7 +150,7 @@ if __name__ == "__main__":
                 observations.pop(0)
 
                 observations_list.append(observations.copy())
-            print(obs)
+            print("myobs", obs)
             print("action", action)
             #print(info)
            # print("space", action_space)
@@ -174,7 +173,6 @@ if __name__ == "__main__":
         #df.to_csv("outOfSample.csv", index=None, header=None, mode="a")
         #df.to_csv("observations.csv", index=None, header=None)
         #df.to_csv("outOfSample.csv", index=None, header=None)
-
 
 
 
