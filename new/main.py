67e94b27ed14817dd4483437be085cf4031b71ce
Simon Lund <simon.lund@campus.lmu.de>
2021-09-03 10:30:50 +0200

    implement a2c and ppo agent from stable baselines, add small setups to test envs, add plot functions

--- new/main.py
+++ new/main.py
@@ -1,50 +1,124 @@
-import usuc
+import math
 import gym
-
+import matplotlib.pyplot as plt
+import numpy as np
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_vec_env
-import data
-import usuc
+
 import neural_net
-
 import reward_functions as rf
+import usuc
 
-def ppo_with_nn():
-    nn = neural_net.load()
 
-    env = gym.make(usuc.USUCEnvWithNN.ID, nn=nn, render=True, time_steps=4, reward_fn=rf.simple)
+def plot(ground_truths, predictions, upper_border=2 * math.pi, lower_border=0):
+    #    TODO
+    data_dir = "./results"
+    pred, std_dev = np.hsplit(predictions, [1])
+    # Make sure, pred and std_dev are 1-d Arrays
+    # TODO Either use np.concatenate or make sure, array is passed with fitting dimensions
+    pred = np.concatenate(pred, axis=None)
+    std_dev = np.concatenate(std_dev, axis=None)
+    fig = plt.figure(figsize=(19, 12))
+
+    plt.plot(ground_truths, label='Ground Truths', color="blue")
+    plt.plot(pred, label='Predictions', color='orange')
+    """
+    higher_dev = [min(p + s, upper_border) for p, s in zip(pred, std_dev)]
+    lower_dev = [max(p - s, lower_border) for p, s in zip(pred, std_dev)]
+    x = range(len(std_dev))
+    plt.fill_between(x, higher_dev, lower_dev, color='yellow', alpha=0.3)
+    plt.plot(higher_dev, color='yellow', alpha=0.5)
+    plt.plot(lower_dev, color='yellow', alpha=0.5)
+    """
+    plt.xlabel("Time")
+    plt.ylabel("Pole Angle")
+    plt.legend()
+    plt.grid()
+    plt.savefig(data_dir + "/plot.png")
+    plt.show()
+    plt.close(fig)
+
+
+def plot_predictions():
+    # TODO
+    data_dir = "./results"
+
+    # load ground truths
+    _, (_, y_test) = gen.load_datasets(data_dir)
+    y_test = list(np.delete(y_test, 0, 1).flat)
+    print(y_test)
+
+    # load predictions
+    predictions = np.genfromtxt(data_dir + "/predictions.csv", delimiter=",")
+
+    # check if data is continuous
+    for i in range(len(y_test) - 1):
+        if abs(y_test[i] - y_test[i + 1]) > 0.5:
+            print(i, y_test[i], y_test[i + 1])
+
+    # plot
+    plot(y_test, predictions)
+
+
+def ppo_test(env, total_timesteps=50000):
     model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=25000)
-    # model.save("ppo_cartpole")
-    # del model  # remove to demonstrate saving and loading
-    # model = PPO.load("ppo_cartpole")
+    model.learn(total_timesteps)
+
+    input("continue?")
 
     for _ in range(10):
-        done = False
         obs = env.reset()
-        while not done:
+        for i in range(1000):
+            print(f"{i}. Step:")
             action, _states = model.predict(obs)
             obs, rewards, done, info = env.step(action)
             env.render()
 
-def ppo_with_original_env():
-    from gym_cartpole_swingup.envs.cartpole_swingup import CartPoleSwingUpV1 as CartPoleSwingUp
+            if done:
+                break
 
+
+def a2c_test(env, total_timesteps=25000):
+    from stable_baselines3 import A2C
+
+    model = A2C("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps)
+
+    input("continue?")
+
+    for _ in range(10):
+        obs = env.reset()
+        for i in range(1000):
+            print(f"{i}. Step:")
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = env.step(action)
+            env.render()
+
+            if done:
+                break
+
+
+def ppo_nn_env():
+    nn = neural_net.load()
+    env = gym.make(usuc.USUCEnvWithNN.ID, nn=nn, render=True, time_steps=4, reward_fn=rf.simple)
+
+    ppo_test(env)
+    a2c_test(env)
+
+
+def ppo_original_env():
+    from gym_cartpole_swingup.envs.cartpole_swingup import CartPoleSwingUpV1 as CartPoleSwingUp
     env = CartPoleSwingUp()
 
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=250000)
-    # model.save("ppo_cartpole")
-    # del model  # remove to demonstrate saving and loading
-    # model = PPO.load("ppo_cartpole")
+    ppo_test(env)
+    a2c_test(env)
 
-    for _ in range(10):
-        done = False
-        obs = env.reset()
-        while not done:
-            action, _states = model.predict(obs)
-            obs, rewards, done, info = env.step(action)
-            env.render()
+
+def ppo_uncertainty_env():
+    env = usuc.USUCEnv()
+
+    ppo_test(env)
+    #a2c_test(env)
+
 
 def run_usuc_with_nn():
     nn = neural_net.load()
@@ -54,26 +128,7 @@ def run_usuc_with_nn():
     usuc.random_actions(env)
 
 
-def ppo_cartpole():
-    import gym
-    env = gym.make('CartPole-v0')
-
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=25000)
-    # model.save("ppo_cartpole")
-    # del model  # remove to demonstrate saving and loading
-    # model = PPO.load("ppo_cartpole")
-
-    for _ in range(10):
-        done = False
-        obs = env.reset()
-        while not done:
-            action, _states = model.predict(obs)
-            obs, rewards, done, info = env.step(action)
-            env.render()
-
-
 if __name__ == '__main__':
     # x, y = data.load("./usuc")
     usuc.register_envs()
-    ppo_with_original_env()
+    ppo_uncertainty_env()
