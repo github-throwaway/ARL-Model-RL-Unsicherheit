605e73514aa34d120a634483fd55406b13792354
Simon Lund <simon.lund@campus.lmu.de>
2021-09-14 16:04:21 +0200

    implement nntest function

--- main.py
+++ main.py
@@ -1,63 +1,14 @@
 import math
+
 import gym
-import matplotlib.pyplot as plt
-import numpy as np
 from stable_baselines3 import PPO
 
+from stable_baselines3 import PPO
+
+import arguments
 import neural_net
 import reward_functions as rf
 import usuc
-import arguments
-
-
-def plot(ground_truths, predictions, upper_border=2 * math.pi, lower_border=0):
-    # TODO
-    data_dir = "./results"
-    pred, std_dev = np.hsplit(predictions, [1])
-    # Make sure, pred and std_dev are 1-d Arrays
-    # TODO Either use np.concatenate or make sure, array is passed with fitting dimensions
-    pred = np.concatenate(pred, axis=None)
-    std_dev = np.concatenate(std_dev, axis=None)
-    fig = plt.figure(figsize=(19, 12))
-
-    plt.plot(ground_truths, label="Ground Truths", color="blue")
-    plt.plot(pred, label="Predictions", color="orange")
-    """
-    higher_dev = [min(p + s, upper_border) for p, s in zip(pred, std_dev)]
-    lower_dev = [max(p - s, lower_border) for p, s in zip(pred, std_dev)]
-    x = range(len(std_dev))
-    plt.fill_between(x, higher_dev, lower_dev, color='yellow', alpha=0.3)
-    plt.plot(higher_dev, color='yellow', alpha=0.5)
-    plt.plot(lower_dev, color='yellow', alpha=0.5)
-    """
-    plt.xlabel("Time")
-    plt.ylabel("Pole Angle")
-    plt.legend()
-    plt.grid()
-    plt.savefig(data_dir + "/plot.png")
-    plt.show()
-    plt.close(fig)
-
-
-def plot_predictions():
-    # TODO
-    data_dir = "./results"
-
-    # load ground truths
-    _, (_, y_test) = gen.load_datasets(data_dir)
-    y_test = list(np.delete(y_test, 0, 1).flat)
-    print(y_test)
-
-    # load predictions
-    predictions = np.genfromtxt(data_dir + "/predictions.csv", delimiter=",")
-
-    # check if data is continuous
-    for i in range(len(y_test) - 1):
-        if abs(y_test[i] - y_test[i + 1]) > 0.5:
-            print(i, y_test[i], y_test[i + 1])
-
-    # plot
-    plot(y_test, predictions)
 
 
 def ppo_test(env, total_timesteps=50000):
@@ -109,17 +60,28 @@ def ppo_nn_env():
 
 
 def nn_test():
+    import evaluation
+
     nn = neural_net.load()
     env = usuc.USUCEnvWithNN(
         nn=nn,
         random_actions=100,
         reward_fn=rf.simple,
-        noise_offset=0,
+        noisy_circular_sector=(0, math.pi),
+        noise_offset=0.5,
+        render=True
     )
 
-    env.reset()
+    env.reset(start_theta=0)
     history = usuc.random_actions(env)
-    print(history[0])
+
+    original_angles = [info["original_theta"] for (_, info) in history]
+    observed_angles = [info["observed_theta"] for (_, info) in history]
+    predicted_angles = [info["predicted_theta"] for (_, info) in history]
+    std = [info["predicted_std"] * 5 for (_, info) in history]
+
+    evaluation.plot_test(original_angles, observed_angles, predicted_angles, std)
+
 
 
 def ppo_original_env():
