2f585383e6dd9fb7a2fc2e4e8b70be53886c849a
Malte Wilhelm <malte.wilhelm93+git@gmail.com>
2021-09-22 10:31:45 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="UNCERT",
+    version="1.0.0",
+    install_requires=[
+        "gym~=0.18.3",
+        "pandas==1.2.5",
+        "matplotlib~=3.4.3",
+        "numpy~=1.19.5",
+        "stable-baselines3",
+        "git+https://github.com/angelolovatto/gym-cartpole-swingup",
+        "dill~=0.3.4",
+        "tqdm~=4.62.2",
+        "gpytorch~=1.5.1",
+        "torch",
+        "scikit-learn~=0.24.2",
+        "blitz-bayesian-pytorch",
+    ],
+    url="https://github.com/github-throwaway/ARL-Model-RL-Unsicherheit",
+    author="Simon Lund, Sophia Sigethy, Georg Staber, and Malte Wilhelm",
+    description="A probabilistic neural network for modeling the system "
+    "dynamics of the Cartpole environment in OpenAI gym. This "
+    "network emulates the environment to train a Reinforcement "
+    "Learning policy. ",
+)
